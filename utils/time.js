a723654f3919d131b172b53b03216986943a5d36
ianarsenault <ian_arsenault@protonmail.com>
2018-08-11 15:10:29 -0400

    fix expiry date check issue

--- utils/time.js
+++ utils/time.js
@@ -1,10 +1,9 @@
+const moment = require('moment')
 
 module.exports = {
 	expired: function(time){
-		let now = new Date().valueOf()
-		console.log(now)	
-		console.log(time)
-		if(parseInt(time) < parseInt(now)){ return true;}
-		else { return false; }		  
+		let now = moment()
+		let expiryDate = moment(time)
+		return (moment(now).isAfter(expiryDate))
 	},
 }
