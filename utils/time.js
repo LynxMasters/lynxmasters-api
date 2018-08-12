79942158b2f1e89982dfb0f07e96315b850ddc44
nmonty9 <namontalbano@email.neit.edu>
2018-08-12 01:42:16 -0400

    fixed DT should have used the library lol

--- utils/time.js
+++ utils/time.js
@@ -1,11 +1,8 @@
 
 module.exports = {
-	expired: function(time){
-		let now = new Date()
-		now.setSeconds(-30)
-		console.log(now)	
-		console.log(time)
-		if(time < now){ return true;}
+	expired: function(time){	
+		console.log(new Date(time).getTime() +"<"+ new Date().getTime())
+		if(new Date(time).getTime() < new Date().getTime()){ return true;}
 		else { return false; }		  
 	},
 }
