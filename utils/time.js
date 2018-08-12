871e4794e7174c19e885dfa5f03f343af905b826
nmonty9 <namontalbano@email.neit.edu>
2018-08-12 13:56:00 -0400

    modified time.js

--- utils/time.js
+++ utils/time.js
@@ -2,7 +2,6 @@
 module.exports = {
 	expired: function(time){	
 		console.log(new Date(time).getTime() +"<"+ new Date().getTime())
-		if(new Date(time).getTime() < new Date().getTime()){ return true;}
-		else { return false; }		  
+		return new Date(time).getTime() < new Date().getTime()	  
 	},
 }
