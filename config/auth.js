23ca06017b0a1b04bdd273a425fc6f6d82f16576
nmonty9 <namontalbano@email.neit.edu>
2018-09-09 14:22:38 -0400

    callback urls

--- config/auth.js
+++ config/auth.js
@@ -7,7 +7,7 @@ module.exports = {
         'clientID'         : 'h9NwYUZGn65RJw',
         'clientSecret'     : 'vOGJ1itvyfWHE_ZxiA5kYKGWl-Q',
         'callbackURL'      : 'https://lynxmasters.com/api/v1/auth/reddit/callback',
-        'authorizeURL': 'https://www.reddit.com/api/v1/authorize?client_id=h9NwYUZGn65RJw&response_type=code&redirect_uri=http://lynxmasters.com/auth/reddit/callback&duration=permanent&scope=identity,edit,flair,history,modconfig,modflair,modlog,modposts,modwiki,mysubreddits,privatemessages,read,report,save,submit,subscribe,vote&state='
+        'authorizeURL': 'https://www.reddit.com/api/v1/authorize?client_id=h9NwYUZGn65RJw&response_type=code&redirect_uri=http://lynxmasters.com/api/v1/auth/reddit/callback&duration=permanent&scope=identity,edit,flair,history,modconfig,modflair,modlog,modposts,modwiki,mysubreddits,privatemessages,read,report,save,submit,subscribe,vote&state='
         // 'callbackURL'      : 'http://localhost:8081/auth/reddit/callback',
        
     },
@@ -16,7 +16,7 @@ module.exports = {
         'clientID'        : 'b83413k7rg3fstv11tx5v7elta4t6l', // your App ID
         'clientSecret'    : 'yj9xcmqdneuaz8kjwqsv6er1p0kxeq', // your App Secret
         'callbackURL'     : 'http://lynxmasters.com/api/v1/auth/twitch/callback',
-        'authorizeURL': 'https://id.twitch.tv/oauth2/authorize?client_id=b83413k7rg3fstv11tx5v7elta4t6l&redirect_uri=http://lynxmasters.com/auth/twitch/callback&response_type=code&scope=user_read+user_subscriptions+user_follows_edit&force_verify=true&state='
+        'authorizeURL': 'https://id.twitch.tv/oauth2/authorize?client_id=b83413k7rg3fstv11tx5v7elta4t6l&redirect_uri=https://lynxmasters.com/api/v1/auth/twitch/callback&response_type=code&scope=user_read+user_subscriptions+user_follows_edit&force_verify=true&state='
         // 'callbackURL'     : 'http://localhost:8081/auth/twitch/callback',
         
     },
