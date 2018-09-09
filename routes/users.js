51669386a4d292650ed8f2d739cb22eb2ab70fea
ianarsenault <ian_arsenault@protonmail.com>
2018-09-09 17:52:10 -0400

    swap node_env for production upload test

--- routes/users.js
+++ routes/users.js
@@ -168,9 +168,9 @@ module.exports = (app) => {
   // Avatar upload
   let storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      let folder = process.env.NODE_ENV === 'production'
-        ? process.env.PROD_UPLOAD_LOCATION
-        : process.env.DEV_UPLOAD_LOCATION
+      let folder = process.env.NODE_ENV === 'dev'
+        ? process.env.DEV_UPLOAD_LOCATION
+        : process.env.PROD_UPLOAD_LOCATION
       cb(null, folder)
     },
     filename: function (req, file, cb) {
